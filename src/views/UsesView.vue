0ab2420c058ef4cb125e2b182373e48cea7f1771
moghwan <chaoui.mar2@gmail.com>
2022-12-11 14:56:18 +0100

    uses section with plain links

--- src/views/UsesView.vue
+++ src/views/UsesView.vue
@@ -8,10 +8,46 @@ import ToolingIcon from '../components/icons/IconTooling.vue'
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Uses TBD</template>
+    <template #heading>
+      Uses
+      (or check the <RouterLink target='_blank' :to="{ name: 'uses' }" class="external-link external-link-right non-tldr-link">!tl;dr</RouterLink> version)
+    </template>
 
     <div class="uses">
-      
+      <h3>OSoftware</h3>
+      <ul>
+        <li>OS: Manjaro i3 community [<a href="https://github.com/moghwan/dotfiles">dotfiles</a>]</li>
+        <li>Dev tools:<ul>
+          <li>PhpStorm &amp; Laradock (LAMP)</li>
+          <li>WebStorm &amp; nvm</li>
+          <li>Vim to edit config files</li>
+        </ul>
+        </li>
+        <li>Stacks:<ul>
+          <li>Laravel / Inertia / Vue 2 / TailWind</li>
+          <li>Laravel / Livewire / Bootstrap 5</li>
+          <li>NuxtJS / TailWind</li>
+          <li>ionic and Kotlin</li>
+        </ul>
+        </li>
+        <li>Deployment: Vercel / Cloudways / OVH (VPS) / Fastlane</li>
+        <li>Terminal: Konsole/iTerm &amp; OhMyZsh [robbyrussell theme]</li>
+        <li>Note-taking: TickTick &amp; Notion.so</li>
+        <li>Browser: Firefox (DE for Desktop, Nightly for mobile)</li>
+      </ul>
+      <h3>Hardware</h3>
+      <ul>
+        <li>Laptop: HP 450 G8</li>
+        <li>Keyboard: Keychron K6, reds with <a href="https://www.reddit.com/r/evangelion/comments/vi5we9">custom keycaps</a></li>
+        <li>Phone: <a href="https://dbrand.com/shop/special-edition/something">Something</a> like Pixel 6 Pro</li>
+        <li>Wearable: miband 6</li>
+      </ul>
+
     </div>
   </WelcomeItem>
 </template>
+
+<style scoped>
+.uses { margin-top: 1rem; }
+.non-tldr-link { background-position-y: .2rem; }
+</style>
