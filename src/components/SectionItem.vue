58c16da3eec9ecac70f5c80f8f47910a313d4f89
moghwan <chaoui.mar2@gmail.com>
2022-12-26 17:43:00 +0100

    separated uses items component

---
+++ src/components/SectionItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>{{ useItem.title }}</h3>
+    <ul>
+      <li v-for="(item, name) in useItem.list" :key="item.id">
+        <span class="bold" v-html="name"></span>:
+        <span v-html="!item.hasSubList ? item.content : null" v-if="!item.hasSubList"></span>
+        <span v-if="item.hasSubList">
+            <ul>
+              <li v-for="(subItem) in item.content" :key="subItem.id">
+                <span v-html="subItem"></span>
+              </li>
+            </ul>
+          </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import {toRefs, defineProps} from 'vue'
+
+const props = defineProps({
+    useItem: {
+      type: Object,
+      required: true
+    }
+})
+// no need to sync since data at loaded on page load
+// const { useItem } = toRefs(props)
+</script>
--- src/views/UsesView.vue
+++ src/views/UsesView.vue
@@ -1,7 +1,8 @@
 <script setup>
 import WelcomeItem from '../components/WelcomeItem.vue'
+import SectionItem from '../components/SectionItem.vue'
 import ToolingIcon from '../components/icons/IconTooling.vue'
-import { uses } from '../assets/data/user.json'
+import {uses} from '../assets/data/user.json'
 
 const uses_ = uses;
 </script>
@@ -9,35 +10,21 @@ const uses_ = uses;
 <template>
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
+      <ToolingIcon/>
     </template>
     <template #heading>
       Uses
-<!--      (or check the <RouterLink target='_blank' :to="{ name: 'uses' }" class="external-link">!tl;dr</RouterLink> version)-->
+      <!--      (or check the <RouterLink target='_blank' :to="{ name: 'uses' }" class="external-link">!tl;dr</RouterLink> version)-->
     </template>
 
     <div class="uses">
-      <p v-for="(use) in uses_" :key="use.id">
-        <h3>{{ use.title }}</h3>
-        <ul>
-          <li v-for="(item, name) in use.list" :key="item.id">
-            <span class="bold" v-html="name"></span>:
-            <span v-html="!item.hasSubList ? item.content : null" v-if="!item.hasSubList"></span>
-            <span v-if="item.hasSubList">
-              <ul>
-                <li v-for="(subItem) in item.content" :key="subItem.id">
-                  <span v-html="subItem"></span>
-                </li>
-              </ul>
-            </span>
-          </li>
-        </ul>
-      </p>
+      <section-item v-for="(use) in uses_" :key="use.id" :useItem="use"/>
     </div>
   </WelcomeItem>
 </template>
 
 <style scoped>
-.uses { margin-top: 1rem; }
-.bold { font-weight: bold }
+.uses {
+  margin-top: 1rem;
+}
 </style>
