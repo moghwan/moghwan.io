a128349c5a47fc76f5e3e466b4529547891fbd6e
moghwan <chaoui.mar2@gmail.com>
2024-02-16 18:39:55 +0100

    vercel analytics removed

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { inject } from '@vercel/analytics';
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
-inject();
 
 app.mount('#app')
